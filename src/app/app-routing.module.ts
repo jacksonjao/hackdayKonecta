58fddfd734662efe295431df8386ba769a4777c5
Jhon Osorio <jhon.osorio.holguin@gmail.com>
2019-10-26 11:05:08 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthComponent} from './core/auth/auth.component';
+import {DashboardComponent} from './modules/dashboard/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'authentication', component: AuthComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
